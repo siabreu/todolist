ac06acb070ec9183bc4acadddd8974634aa8ebe8
siabreu <siabreu@gmail.com>
2023-06-05 15:54:42 -0300

    changes routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const tasksController = require('./controller/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
 
 const router = express.Router();
 
+router.get('/tasks', tasksController.getAll);
 
-router.get('/tasks', (req, res) => {
-    
-});
+router.post('/tasks', tasksMiddleware.validateFieldTitle, tasksController.createTask);
 
+router.delete('/tasks/:id', tasksController.deleteTask);
+
+router.put('/tasks/:id',
+  tasksMiddleware.validateFieldTitle,
+  tasksMiddleware.validateFieldStatus,
+  tasksController.updateTask
+);
 
 module.exports = router;
