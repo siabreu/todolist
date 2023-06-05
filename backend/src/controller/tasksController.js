f64551e5cd55e33d035294b7a133e6aee57f1644
siabreu <siabreu@gmail.com>
2023-06-05 15:53:41 -0300

    adds controller to tasks

---
+++ backend/src/controller/tasksController.js
@@ -0,0 +1,31 @@
+const tasksModel = require('../models/tasksModel');
+
+const getAll = async (_request, response) => {
+  const tasks = await tasksModel.getAll();
+
+  return response.status(200).json(tasks);
+};
+
+const createTask = async (request, response) => {
+  const crestedTask = await tasksModel.createTask(request.body);
+  return response.status(201).json(crestedTask);
+};
+
+const deleteTask = async (request, response) => {
+  const { id } = request.params;
+  await tasksModel.deleteTask(id);
+  return response.status(204).json();
+};
+
+const updateTask = async (request, response) => {
+  const { id } = request.params;
+  await tasksModel.updateTask(id, request.body);
+  return response.status(204).json();
+};
+
+module.exports = {
+  getAll,
+  createTask,
+  deleteTask,
+  updateTask
+};
