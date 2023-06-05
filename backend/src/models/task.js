fe392d6e1b9202c07b61db6c2fef77418c0211a5
siabreu <siabreu@gmail.com>
2023-06-05 17:48:17 -0300

    changes methods to sequelize

--- backend/src/models/task.js
+++ backend/src/models/task.js
@@ -1,4 +1,3 @@
-
 const Sequelize = require('sequelize');
 const database = require('../../db');
 
--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,33 +1,34 @@
-const connection = require('./connection');
+const database = require('../../db');
+const Task = require('../models/task');
 
 const getAll = async () => {
-  const [tasks] = await connection.execute('SELECT * FROM tasks');
-  return tasks;
+  await database.sync();
+  return await Task.findAll();
 };
 
 const createTask = async (task) => {
-  const { title } = task;
-  const dataUTC = new Date(Date.now()).toUTCString();
-  
-  const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
-  const [createdTask] = await connection.execute(query, [title, 'pendente', dataUTC]);
-  
-  return { insertId: createdTask.insertId };
-};
-
-const deleteTask = async (id) => {
-  const removedTask = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
-  return removedTask;
+  await Task.create({
+    title: task.title,
+    status: 'pendente'
+  });
 };
 
 const updateTask = async (id, task) => {
-  const { title, status } = task;
-  const dataUTC = new Date(Date.now()).toUTCString();
+  const updatedTask = await Task.findByPk(id);
+  console.log('find by id: ', updatedTask);
+  updatedTask.set({
+    title: task.title,
+    status: task.status
+  });
 
-  const query = 'UPDATE tasks SET title = ?, status = ?, updated_at = ? WHERE id = ?';
-  const [updatedTask] = await connection.execute(query, [title, status, dataUTC, id]);
-
+  await updatedTask.save();
   return updatedTask;
+};
+
+const deleteTask = async (id) => {
+  const removedTask = await Task.findByPk(id);
+  await removedTask.destroy();
+  return removedTask;
 };
 
 module.exports = {
